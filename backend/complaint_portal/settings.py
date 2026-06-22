6a95725480431a874e3d903403f1eecb597e61d4
hafsa-suleman-kodifly <hafsa.suleman@kodifly.com>
2026-06-22 09:55:37 +0500

    api issue resolved

--- backend/complaint_portal/settings.py
+++ backend/complaint_portal/settings.py
@@ -46,6 +46,7 @@ CORS_ALLOWED_ORIGINS = [
     "http://localhost:5173",
     "http://127.0.0.1:5173",
     "https://many-monotype-ascend.ngrok-free.dev",
+    "https://complaint-portal-7423-mdw9akxfr-hafsasuleman-6301s-projects.vercel.app",
 ]
 
 
