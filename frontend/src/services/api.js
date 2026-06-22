be2ede47b03d943bdcc856e4a2d77fbc3df1265e
hafsa-suleman-kodifly <hafsa.suleman@kodifly.com>
2026-06-22 09:30:44 +0500

    fixed env var

--- frontend/src/services/api.js
+++ frontend/src/services/api.js
@@ -1,11 +1,10 @@
 import axios from "axios";
 
+console.log("API URL:", import.meta.env.VITE_API_BASE_URL);
 
 const api = axios.create({
     baseURL: import.meta.env.VITE_API_BASE_URL,
-
 });
-
 
 api.interceptors.request.use(
 (config)=>{
